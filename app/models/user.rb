8536c653f23ae6f484da2cacc4e23498b134ea49
Bodaniel Jeanes <me@bjeanes.com>
2008-10-05 22:09:48 +1000

    Removed email and name attribute from authenticated system

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,20 +10,12 @@ class User < ActiveRecord::Base
   validates_uniqueness_of   :login
   validates_format_of       :login,    :with => Authentication.login_regex, :message => Authentication.bad_login_message
 
-  validates_format_of       :name,     :with => Authentication.name_regex,  :message => Authentication.bad_name_message, :allow_nil => true
-  validates_length_of       :name,     :maximum => 100
-
-  validates_presence_of     :email
-  validates_length_of       :email,    :within => 6..100 #r@a.wk
-  validates_uniqueness_of   :email
-  validates_format_of       :email,    :with => Authentication.email_regex, :message => Authentication.bad_email_message
-
   
 
   # HACK HACK HACK -- how to do attr_accessible from here?
   # prevents a user from submitting a crafted form that bypasses activation
   # anything else you want your user to change should be added here.
-  attr_accessible :login, :email, :name, :password, :password_confirmation
+  attr_accessible :login, :password, :password_confirmation
 
 
 
@@ -42,13 +34,4 @@ class User < ActiveRecord::Base
   def login=(value)
     write_attribute :login, (value ? value.downcase : nil)
   end
-
-  def email=(value)
-    write_attribute :email, (value ? value.downcase : nil)
-  end
-
-  protected
-    
-
-
 end
--- db/migrate/20081005120038_create_users.rb
+++ db/migrate/20081005120038_create_users.rb
@@ -2,8 +2,6 @@ class CreateUsers < ActiveRecord::Migration
   def self.up
     create_table "users", :force => true do |t|
       t.column :login,                     :string, :limit => 40
-      t.column :name,                      :string, :limit => 100, :default => '', :null => true
-      t.column :email,                     :string, :limit => 100
       t.column :crypted_password,          :string, :limit => 40
       t.column :salt,                      :string, :limit => 40
       t.column :created_at,                :datetime

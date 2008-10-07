6569acce037d8fdfd547d6fb60fbc9ab15c1b6a7
Bodaniel Jeanes <me@bjeanes.com>
2008-10-08 00:55:35 +1000

    filled in some stream controller code

--- app/controllers/stream_controller.rb
+++ app/controllers/stream_controller.rb
@@ -1,20 +1,33 @@
 class StreamController < ApplicationController
-  %w{part disconnect}.each do |action|
-    define_method(action) do
-      render :text => ""
-    end
+  def part
+    # This should remove us from a game/channel
+  end
+  
+  def disconnect
+    request_user.offline! if request_valid?
   end
   
   def join
-    session = session_data_by_id(params[:session_id])
-    unless session && session[:user_id].to_i == params[:client_id].to_i
-      logger.debug("[Juggernaut] client tried to connect with invalid session_id")
-      logger.debug("  session:    #{session.inspect}")
-      render :text => "403", :status => 403
-    else
-      user = User.find(session[:user_id])
-      user.online!
+    if request_valid?
+      request_user.online!
+      
+      # This should add the request_user to a game/lobby
+      
       render :nothing => true
+    else
+      render :text => "403", :status => 403
     end
+  end
+  
+  private
+  def request_valid?
+    @request_valid ||= begin
+      @session = session_data_by_id(params[:session_id])
+      @session && @session[:user_id].to_i == params[:client_id].to_i
+    end
+  end
+  
+  def request_user
+    @user ||= User.find(@session[:user_id]) if request_valid?
   end
 end

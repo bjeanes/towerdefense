51cb2f1a6d670357f88e9be231a1cf30d39ba815
Bodaniel Jeanes <me@bjeanes.com>
2008-10-16 19:31:43 +1000

    Added some named scopes for finding open/concluded/active games (for display in lobby, etc)

--- app/models/game.rb
+++ app/models/game.rb
@@ -4,6 +4,15 @@ class Game < ActiveRecord::Base
   
   validates_associated :playerships
   
+  # Games that can be joined
+  named_scope :open, :conditions => 'started_at IS NULL', :include => :players
+  
+  # Games that are being played but have not finished
+  named_scope :active, :conditions => 'concluded_at IS NULL AND started_at IS NOT NULL', :include => :players
+  
+  # Games that have finished
+  named_scope :concluded, :conditions => 'concluded_at IS NULL'
+  
   def concluded?
     !concluded_at.nil?    
   end

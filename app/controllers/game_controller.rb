b4cc2b7092625bede7c391fa69bf4f1fd659a9a8
Bodaniel Jeanes <me@bjeanes.com>
2008-10-16 20:38:56 +1000

    Really fixed game index user_list partial path. Added a few TODOs

--- app/controllers/game_controller.rb
+++ app/controllers/game_controller.rb
@@ -10,12 +10,15 @@ class GameController < ApplicationController
     redirect_to lobby_path
   end
   
+  # TODO - make sure we are removed from all other games before joining a new one
   def join
     game = Game.find(params[:id])
     game.join(current_user)
     
     redirect_to game_path
   rescue
+    # TODO - redirect to game_path if we are already in this game, otherwise lobby_path
+    
     flash[:error] = "That is not a game that can be joined"
     redirect_to lobby_path
   end
